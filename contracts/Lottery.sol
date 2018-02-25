b1d2bbec8d90598fbb9463affc1e3c26aa6fccaf
rja907 <kmrrajj@gmail.com>
2018-02-25 18:59:04 +0530

    added lottery players

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Lottery{
     address public manager;
+    address[] public players;
     function Lottery() public {
         manager = msg.sender; //someone who starts the lottery
+    }
+    function enter() public payable {
+        require(msg.value > .01 ether); //minimum amount to enter lottery
+        players.push(msg.sender); //adding a new player to the lottery
     }
 }
